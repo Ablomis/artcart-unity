21b11f081fe91d440aa28f59972765184051f66e
Anatolii <ablomis@gmail.com>
2022-03-29 09:50:32 -0500

    added awardNFT function

--- functions/awardSpecificNFT.js
+++ functions/awardSpecificNFT.js
@@ -12,26 +12,6 @@ exports.handler = async (event, context, callback) => {
   });
   const token = responseToken.data.access_token;
 
-  const responseTemplates = await axios.get("https://platform.artcart.cloud/api/transactional/templates", 
-    { headers: { "Authorization": `Bearer ${token}` } });
-  const templatesList = responseTemplates.data;
-  const templatesNumber = templatesList.length;
-
-  let eligibleTemplates = [];
-
-  for (i = 0; i<templatesNumber; i++){
-    if(templatesList[i].limit === 0 || templatesList[i].count < templatesList[i].limit)
-    {
-        eligibleTemplates.push(templatesList[i]);
-    }
-    //ADD ANY CUSTOM LOGIC HERE
-    /*
-    //------------------------
-    */
-  }
-
-  
-
   const responseNFT = await axios.post(`https://platform.artcart.cloud/api/transactional/nft`, {
     cid: data.cid,
     email: data.email
